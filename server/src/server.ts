3a9ed85b5c9be013724af1abb835cf6b057057ad
Gero Kassing <geroalexander@Macbooks-MacBook-Pro.local>
2023-04-10 21:09:29 +0200

    fix(server): amend entry

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,39 +1,13 @@
-import express, { Request, Response } from "express";
-import { BlogPost } from "./interfaces";
+import express from "express";
 
 const app = express();
+const router = express.Router();
 
 // Middleware
 app.use(express.json());
 
 // Routes
-let posts: BlogPost[] = [];
-
-// Create a new blog post
-app.post("/posts", (req: Request, res: Response) => {
-  const { title, body } = req.body;
-  const timestamp = Date.now();
-  const id = Math.floor(Math.random() * 1000000).toString();
-  const post: BlogPost = { id, title, body, timestamp };
-  posts.push(post);
-  res.status(201).json(post);
-});
-
-// Retrieve all blog posts
-app.get("/posts", (req: Request, res: Response) => {
-  res.json(posts);
-});
-
-// Retrieve a single blog post by ID
-app.get("/posts/:id", (req: Request, res: Response) => {
-  const { id } = req.params;
-  const post = posts.find((p) => p.id === id);
-  if (!post) {
-    res.status(404).json({ error: "Post not found" });
-  } else {
-    res.json(post);
-  }
-});
+app.use("/", router);
 
 // Start the server
 const port = 3000;
