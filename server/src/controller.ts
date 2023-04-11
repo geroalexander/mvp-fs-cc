3c49342c2693753176edbdacd3bbb9e614409ea8
geroalexander <gero.a.kassing@gmail.com>
2023-04-11 12:01:46 +0200

    fix: add status code 404

--- server/src/controller.ts
+++ server/src/controller.ts
@@ -4,7 +4,11 @@ import { BlogPost } from "./interfaces";
 let posts: BlogPost[] = [];
 
 export const getAllPosts = async (req: Request, res: Response) => {
-  res.json(posts);
+  if (posts.length === 0) {
+    res.status(404).json({ message: "No posts found." });
+  } else {
+    res.json(posts);
+  }
 };
 
 export const createNewPost = async (req: Request, res: Response) => {
@@ -13,7 +17,11 @@ export const createNewPost = async (req: Request, res: Response) => {
   const id = Math.floor(Math.random() * 1000000).toString();
   const newPost: BlogPost = { id, title, body, timestamp };
   posts.push(newPost);
-  res.status(201).json(newPost);
+  if (!newPost) {
+    res.status(404).json({ message: "Post not found." });
+  } else {
+    res.status(201).json(newPost);
+  }
 };
 
 export const getOnePostById = async (req: Request, res: Response) => {
